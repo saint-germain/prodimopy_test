7b031e0aaa90ad32e92f9c1aea2a10ff591e1eda
Christian Rab <christian.rab@univie.ac.at>
2015-10-30 12:12:53 +0100

    New plots and smaller changes
    - new plot_avgabun and plot_midplane
      these plots work for a list of species 
      
    - adaptations for plot_cont
    
    - added _dokwargs and _dolegend: not yet used everywhere

--- prodimopy/prodimopy/plot.py
+++ prodimopy/prodimopy/plot.py
@@ -50,13 +50,58 @@ def plog(array):
   return array
 
 class Plot(object):
-  def __init__(self, pdf, fs_legend=6):
+  def __init__(self, pdf, fs_legend=7.5):
     self.pdf = pdf
     self.fs_legend = fs_legend
+    self.ncol_legend = 5
   
+  def _legend(self, ax,loc="best"):
+    '''
+    plots the legend, deals with multiple columns
+    '''
+    handles, labels = ax.get_legend_handles_labels()
+    ncol = 1
+    if self.ncol_legend > 1 and len(labels) > self.ncol_legend:
+      ncol = int(len(labels) / self.ncol_legend)
+    
+    ax.legend(handles, labels, loc=loc, fancybox=False, ncol=ncol, fontsize=self.fs_legend)
+  
+  
+  def _dokwargs(self,ax,**kwargs):
+    '''
+    Handles the passed kwargs elements (assumes that defaults are already set)
+    '''
+    if "ylim" in kwargs: 
+      ax.set_ylim(kwargs["ylim"])
+      
+    if "xlim" in kwargs: 
+      ax.set_xlim(kwargs["xlim"])
+              
+    if "xlog" in kwargs:
+      if kwargs["xlog"]: 
+        ax.semilogx()
+      else:
+        ax.set_xscale("linear")
+      
+    if "ylog" in kwargs:
+      if kwargs["ylog"]: 
+        ax.semilogy()
+      else:              
+        ax.set_yscale("linear")
+      
+    if "xlabel" in kwargs:
+      ax.set_xlabel(kwargs["xlabel"])  
+
+    if "ylabel" in kwargs:
+      ax.set_ylabel(kwargs["ylabel"])
+      
+    if "title" in kwargs:
+      if kwargs["title"].strip() != "":
+        ax.set_title(kwargs["title"])        
+
   
   def plot_cont(self, model, values, label="value", zlog=True, 
-                zlim=[None, None],zr=True,**kwargs):
+                zlim=[None, None],zr=True,clevels=None,contour=True,plot_outfile=None,**kwargs):
   
     if zlog is True:
       pvals = plog(values)
@@ -97,9 +142,9 @@ class Plot(object):
     CS = ax.contourf(x, y, pvals, levels=levels, cmap=cmap)
     # This is the fix for the white lines between contour levels
     for c in CS.collections:
-      c.set_edgecolor("face")
-    
+      c.set_edgecolor("face")    
 
+    # FIXME: use dokwargs here
     if "xlim" in kwargs:
       ax.set_xlim(kwargs["xlim"])      
     else:    
@@ -108,7 +153,6 @@ class Plot(object):
       ax.set_ylim(kwargs["ylim"])    
     else:  
       ax.set_ylim([y.min(), y.max()])
-
     
     if not "xlog" in kwargs:
       ax.semilogx()
@@ -125,17 +169,32 @@ class Plot(object):
     else:
       ax.set_ylabel("z [AU]")      
     
-    CB = fig.colorbar(CS, ticks=ticks)
+    if "title" in kwargs and kwargs["title"].strip() != "":      
+      ax.set_title(kwargs["title"])        
+      #ax.text(0.27, 0.95,kwargs["title"], horizontalalignment='center',
+      #   verticalalignment='center',fontsize=8,
+      #   transform=ax.transAxes)            
+   
+    CB = fig.colorbar(CS, ax=ax,ticks=ticks,pad=0.01)
+    CB.ax.tick_params(labelsize=self.fs_legend) 
     # CB.set_ticks(ticks)
-    CB.set_label(label)  
+    CB.set_label(label,fontsize=self.fs_legend)  
     
-    ax.contour(CS, levels=ticks, colors='black', linestyles="dashed")
+    if contour:
+      if clevels != None:
+        if zlog: clevels=np.log10(clevels)
+        ax.contour(CS, levels=clevels, colors='black', linestyles="solid",linewidths=1.0)
+      else:
+        ax.contour(CS, levels=ticks, colors='black', linestyles="dashed",linewidths=1.0)
     
-    self.pdf.savefig()
+    if plot_outfile!= None:
+      fig.savefig(plot_outfile)
+    else:
+      self.pdf.savefig(transparent=False)
     plt.close(fig)
   
-  def plot_ionrates_midplane(self, model, **kwargs):
-                       
+  
+  def plot_ionrates_midplane(self, model, **kwargs):                       
     
     x = np.log10(model.x[:,0])      
 
@@ -170,9 +229,9 @@ class Plot(object):
     ax.semilogy()     
     
     handles, labels = ax.get_legend_handles_labels()
-    ax.legend(handles, labels, loc="best", fancybox=True, framealpha=0.5)    
+    ax.legend(handles, labels, loc="best", fancybox=True)    
       
-    self.pdf.savefig()
+    self.pdf.savefig(transparent=False)
     plt.close(fig)  
 
   
@@ -225,3 +284,98 @@ class Plot(object):
       
     self.pdf.savefig()
     plt.close(fig)  
+
+  def plot_avgabun(self,model,species,**kwargs):
+    '''
+    Plots the average abundance for the given species (can be more than one) 
+    as a function of radius    
+    '''
+    print("PLOT: plot_avgabun ...")     
+          
+    fig, ax = plt.subplots(1, 1)     
+  
+    iplot = 0
+    for spec in species:                 
+    # get the species
+      if (spec in model.spnames):   
+        y=model.cdnmol[:,0,model.spnames[spec]]
+        y=y/model.NHver[:,0]   
+        x = model.x[:,0]            
+        
+        style="-"
+        if "#" in spec: style="--"
+        ax.plot(x, y, linestyle=style, marker=None, label="$\mathrm{"+spnToLatex(spec)+"}$")
+    
+        iplot = iplot + 1              
+    
+    if iplot==0:
+      print("Species "+species+ " not found in any model!")
+      return 
+            
+    ax.set_xlabel(r"r [AU]")
+    ax.set_ylabel(r"average abundance")
+    ax.set_xlim([x.min(),x.max()])
+    
+    # do axis style
+    ax.semilogy()     
+    
+    self._dokwargs(ax,**kwargs)    
+    self._legend(ax)
+    
+    self.pdf.savefig()
+    plt.close(fig)
+    
+
+  def plot_abun_midp(self, model,species, norm=None,styles=None,colors=None, **kwargs):
+    '''
+    Plots the abundances in the midplane for the given species (can be more than one)
+    norm .. normalisation constant (additional to Nhto e.g. the total carbon abundance)   
+    '''
+    print("PLOT: plot_abun_midp ...")
+    fig, ax = plt.subplots(1, 1)      
+    
+    iplot = 0
+    xmin = 1.e100
+    xmax = 0
+    ymin = 1.e100
+    ymax = -1.e00 
+    for spec in species:           
+      x = model.x[:, 0]
+      y = model.nmol[:,0,model.spnames[spec]]/model.nHtot[:,0]
+      if norm != None:
+        y=y/norm                    
+      
+      # FIXME: add proper treatment for styles and colors 
+      
+      if styles==None:         
+        style="-"
+        if "#" in spec: style="--"
+      else: 
+        style=styles[iplot]
+        
+      if colors==None:
+        color=None
+      else:
+        color=colors[iplot]  
+        
+      ax.plot(x, y, marker=None, linestyle=style, color=color, label="$\mathrm{"+spnToLatex(spec)+"}$")
+                      
+      iplot = iplot + 1
+      
+      if min(x) < xmin: xmin = min(x)
+      if max(x) > xmax: xmax = max(x)
+      if min(y) < ymin: ymin = min(y)
+      if max(y) > ymax: ymax = max(y)
+  
+    ax.set_xlim(xmin,xmax)
+    ax.set_ylim(ymin, ymax)              
+    ax.semilogy()
+    ax.set_xlabel("r [AU]")
+    ax.set_ylabel(r"$\mathrm{\epsilon(X)}$")
+            
+    self._dokwargs(ax, **kwargs)
+    self._legend(ax, loc="lower left") 
+  
+    self.pdf.savefig()
+    plt.close(fig)
+
