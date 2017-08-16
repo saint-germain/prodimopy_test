7a0201324edb5c3a3a54131ed930e1ce35298b6f
Christian Rab <rab@astro.rug.nl>
2017-08-16 09:51:34 +0200

    Another try for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,7 @@
 #
 import os
 import sys
-print(os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(1, os.path.abspath('../'))
 
 
 # -- General configuration ------------------------------------------------
