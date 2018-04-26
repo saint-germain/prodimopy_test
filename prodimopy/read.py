6742ea00469a2c3f8c10da490f80b99deb2a647e
Christian Rab <rab@astro.rug.nl>
2018-04-26 19:53:15 +0200

    removed some whitespace

--- prodimopy/read.py
+++ prodimopy/read.py
@@ -609,7 +609,7 @@ class DataLine(object):
     res=self.flux*u.Watt/(u.m**2.0)
     ckm=const.c.to('km/s')
        
-    res=(res).to(u.Jansky,equivalencies=u.spectral_density(self.frequency*u.GHz))  
+    res=(res).to(u.Jansky,equivalencies=u.spectral_density(self.frequency*u.GHz))
       
     return (res*ckm).value
 
