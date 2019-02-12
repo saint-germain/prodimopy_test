594c93a3c755335189b572b81f6eaffc7878bdb2
Christian Rab <christian.rab@univie.ac.at>
2019-02-12 17:22:30 +0100

    update requirements for astropy and matplotlib

--- setup.py
+++ setup.py
@@ -69,10 +69,10 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     # FIXME: include proper version numbers
     install_requires=[
-                      'matplotlib',
-                      'numpy',
-                      'scipy',
-                      'astropy>=1.3,<3'],
+                      'astropy>2'
+                      'matplotlib>2',
+                      'numpy',   # no special requirements, but astropy has some
+                      'scipy'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
